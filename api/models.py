2c2fa49864d5e95a7abdcdef2fb8f42e0ef1586e
Ashley Sands <ashley.sands01@gmail.com>
2024-08-23 09:37:14 -0400

    add blank true to date, description, and trip_length

--- api/models.py
+++ api/models.py
@@ -4,9 +4,9 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Trip(models.Model):
     location = models.CharField(max_length=100)
-    trip_length = models.IntegerField()
-    date = models.DateField()
-    description = models.TextField()
+    trip_length = models.IntegerField(blank=True)
+    date = models.DateField(blank=True)
+    description = models.TextField(blank=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="trips")
 
     def __str__(self):
